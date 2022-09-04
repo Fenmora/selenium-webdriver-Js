3c4d72c8da9fe00f0b3d043bd9516666384be7bb
fmora <fmora@cmnhospitals.org>
2022-09-04 17:40:35 -0600

    using mocha with js

--- test/firstTest.js
+++ test/firstTest.js
@@ -5,35 +5,41 @@
  ** Description: Testing to the pase
  *? https://lambdatest.github.io/sample-todo-app/
  */
-
 const { Builder, By, Key } = require("selenium-webdriver");
 const assert = require("assert");
+var should = require("chai").should();
+
+//describe
+describe("add to do test", function () {
+  //it block
+  it("successfully adds a todo application", async function () {
+    // launch the browser
+    let driver = await new Builder().forBrowser("chrome").build();
+
+    //navigate to out application
+    await driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+    //add a todo
+    await driver
+      .findElement(By.id("sampletodotext"))
+      .sendKeys("Fender Mora - Selenium", Key.RETURN);
+
+    let todoText = await driver
+      .findElement(By.xpath("//li[last()]"))
+      .getText()
+      .then(function (value) {
+        return value;
+      });
+
+    //assert using chai should
+    todoText.should.equal("Fender Mora - Selenium");
+
+    //Close Browser
+    await driver.quit();
+  });
+  //
+});
 
 async function example() {
-  // launch the browser
-  let driver = await new Builder().forBrowser("chrome").build();
-
-  //navigate to out application
-  await driver.get("https://lambdatest.github.io/sample-todo-app/");
-
-  //add a todo
-  await driver
-    .findElement(By.id("sampletodotext"))
-    .sendKeys("Fender Mora - Selenium", Key.RETURN);
-
-  //assert
-  let todoText = await driver
-    .findElement(By.xpath("//li[last()]"))
-    .getText()
-    .then(function (value) {
-      return value;
-    });
-
-  assert.strictEqual(todoText, "Fender Mora - Selenium");
-  //Close Browser
-  await driver.quit();
-
   //close the browser
 }
-
-example();
