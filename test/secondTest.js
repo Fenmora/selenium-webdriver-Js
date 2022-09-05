f444aff70d34bb1ae516bcb02740ae6fcab0689c
fmora <fmora@cmnhospitals.org>
2022-09-04 18:19:41 -0600

    parallelization using mocha

---
+++ test/secondTest.js
@@ -0,0 +1,42 @@
+/**
+ ** @Author: Fender Mora
+ ** Date  : September 3rd
+ *
+ ** Description: Testing to the pase
+ *? https://lambdatest.github.io/sample-todo-app/
+ *! to run : npx mocha -no-timeouts 'test/*.js' o npx test
+ */
+const { Builder, By, Key } = require("selenium-webdriver");
+const assert = require("assert");
+var should = require("chai").should();
+
+//describe
+describe("add to do test - Second Task", function () {
+  //it block
+  it("successfully adds a todo application", async function () {
+    // launch the browser
+    let driver = await new Builder().forBrowser("chrome").build();
+
+    //navigate to out application
+    await driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+    //add a todo
+    await driver
+      .findElement(By.id("sampletodotext"))
+      .sendKeys("Fender Mora - Selenium", Key.RETURN);
+
+    let todoText = await driver
+      .findElement(By.xpath("//li[last()]"))
+      .getText()
+      .then(function (value) {
+        return value;
+      });
+
+    //assert using chai should
+    todoText.should.equal("Fender Mora - Selenium");
+
+    //Close Browser
+    await driver.quit();
+  });
+  //
+});
